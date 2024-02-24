48bcf173cb6d529a0e37cbe69c45ec9d9eea1e54
eby <78696494+Ebertug@users.noreply.github.com>
2024-02-25 00:29:41 +0300

    leetCode217

---
+++ containsDuplicate.py
@@ -0,0 +1,11 @@
+def containsDuplicate(self,nums):
+    return not len(set(nums)) ==len(nums)
+###
+testCases=[
+    [1,2,3,1],
+    [1,2,3,4],
+    [1,1,1,3,3,4,3,2,4,2]
+]
+###
+for nums in testCases:
+    containsDuplicate(nums)
